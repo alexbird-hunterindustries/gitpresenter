f012e5c2ea7935f01910509a1e66455b631bd968
Alex Bird <alex.bird@hunter.global>
2025-01-17 15:09:35 -0800

    Update the dev script so it can be run from other directories (to try it with different git repos)

--- dev.mjs
+++ dev.mjs
@@ -2,7 +2,7 @@
 
 import { execSync } from 'node:child_process';
 
-console.log(execSync('npm run build').toString());
+console.log(execSync('npm run build', { cwd: import.meta.dirname }).toString());
 
 const { main } = await import('./dist/main.js');
 
