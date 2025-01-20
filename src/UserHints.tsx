e7de07c34b0a64d50b0c5a18a3233e7a1fe7d512
Alex Bird <alex.bird@hunter.global>
2025-01-20 08:06:42 -0800

    F** Add a user hint about the starting branch

--- src/UserHints.tsx
+++ src/UserHints.tsx
@@ -20,7 +20,8 @@ export const UserHints = ({ width }: { width: number }) => {
 const hints = [
   'Tag a commit with "gitpresenter-start" to automatically start your presentation at that commit',
   'Use your git commit descriptions as "presenter notes" for each slide',
-  'When gitpresenter checks out a commit, it does a soft reset so your IDE can show the commit diff'
+  'When gitpresenter checks out a commit, it does a soft reset so your IDE can show the commit diff',
+  'gitpresenter reads commit starting from your git HEAD. If you can\'t see your commits, check out your main branch before starting gitpresenter.'
 ]
 
 const randomHint = hints[Math.round(Math.random() * (hints.length - 1))];
