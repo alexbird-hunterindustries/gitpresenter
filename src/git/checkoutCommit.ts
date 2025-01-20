c08a1d31d270d0dffbaf5406211ec2fb2a122811
Alex Bird <alex.bird@hunter.global>
2025-01-20 11:56:40 -0800

    F** Update checkoutCommit.ts so that it only attempts to reset --soft if there is a previous commit

--- src/git/checkoutCommit.ts
+++ src/git/checkoutCommit.ts
@@ -6,5 +6,16 @@ const exec = promisify(execWithCallback);
 export async function checkoutCommit(hash: string) {
   await exec(`git reset --hard HEAD`);
   await exec(`git checkout ${hash}`);
-  await exec(`git reset --soft HEAD^`);
+  if (await doesRefExist('HEAD^')) {
+    await exec(`git reset --soft HEAD^`);
+  }
+}
+
+async function doesRefExist(ref: string) {
+  try {
+    await exec(`git rev-list ${ref} -n 1`);
+    return true;
+  } catch (ignored) {
+    return false;
+  }
 }
